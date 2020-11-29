0afa716e22eb5158bd186876417104ac391f9c8d
BINX-PC\bin <nihon_binx@hotmail.com>
2020-11-28 21:36:21 -0300

    Started
    Criação de files index e style

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>LandingPage</title>
+  <link rel="stylesheet" href="css/style.css">
+  <meta http-equiv="refresh" content="30; URL='index.php'"/>
+</head>
+  <body>
+    <div class="menu-btn">
+      <div class="menu-btn__burger">
+      </div>
+      <script src="js/main.js"></script>
+    </div>
+  </body>
+</html>
